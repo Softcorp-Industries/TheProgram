ddd7c346b33f4b2963e6648e9e068081a4b898eb
Christopher Guy <softcorpindustries@gmail.com>
2022-01-30 09:02:20 -0700

    Updates

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as DarkMode from './js/darkMode.js';
-import * as NavBar from './js/navBar.js';
+import * as NavBar from './js/nav.js';
 import * as Greeting from './js/greeting.js';
 
 // import * as Header from "./js/header.js";
