6360ba71df6867595c94a1fab362a50dff98f2f3
Christopher Guy <softcorpindustries@gmail.com>
2022-04-22 21:53:29 -0600

    landing

--- dist/js/darkMode.js
+++ dist/js/darkMode.js
@@ -1,5 +1,8 @@
 // Dark Mode and Local Storage
 const darkModeBtn = document.getElementById("darkModeBtn");
+const darkModeLanding = document.getElementById("darkModeLanding");
+
+
 let darkModeSt = localStorage.getItem("darkModeSt");
 let darkModeOn = false;
 const enableDarkMode = () => {
@@ -7,6 +10,7 @@ const enableDarkMode = () => {
   darkModeBtn.style.backgroundImage = "url(img/icon/sun_light_mode_day-2-512.webp)"
   darkModeBtn.classList.add("darkModeAnim");
   darkModeBtn.classList.remove("darkModeAnimRev");
+  darkModeLanding.classList.add("darkModeFilter");
   localStorage.setItem("darkModeSt", "enabled");
 };
 const disableDarkMode = () => {
@@ -14,6 +18,7 @@ const disableDarkMode = () => {
   darkModeBtn.style.backgroundImage = "url(img/icon/936949-200.png)"
   darkModeBtn.classList.add("darkModeAnimRev");
   darkModeBtn.classList.remove("darkModeAnim");
+  darkModeLanding.classList.remove("darkModeFilter");
   localStorage.setItem("darkModeSt", "disabled");
 };
 if (darkModeSt === "enabled") {
